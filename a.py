6214ef8216a41a2fc702762db86848d3f582b155
Vivek Gupta <vivekgu@google.com>
2021-04-16 19:19:56 -0700

    Line1

---
+++ a.py
@@ -0,0 +1,17 @@
+"""TODO(vivekgu): DO NOT SUBMIT without one-line documentation for a.
+
+TODO(vivekgu): DO NOT SUBMIT without a detailed description of a.
+"""
+
+from typing import Sequence
+
+from absl import app
+
+
+def main(argv: Sequence[str]) -> None:
+  if len(argv) > 1:
+    raise app.UsageError('Too many command-line arguments.')
+
+
+if __name__ == '__main__':
+  app.run(main)
